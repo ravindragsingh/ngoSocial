2d86112dc6981a64745a44241847db93a63a8bd9
ravindragsingh <arien.641@gmail.com>
2019-07-14 00:39:58 -0400

    dashboardAPICalls

--- src/app/login/common/dashboard/dashboard.component.ts
+++ src/app/login/common/dashboard/dashboard.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-dashboard',
@@ -6,10 +8,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
+  responseProject: any;
+  donorProfile: any;
+  responseDonor: any;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute, private http: HttpClient) { }
 
   ngOnInit() {
+    let responseProject = this.http.get('http://smtb.mybluemix.net/get-project-info?projectId=project001');
+    
+    responseProject.subscribe(
+      (retValProj) => {
+        this.responseProject = retValProj;
+        console.log(retValProj)
+      }
+     );
+
+     let donorProfile = this.http.get('http://smtb.mybluemix.net/get-user?userId=surendra&userType=donor');
+    
+     donorProfile.subscribe(
+       (donorProf) => {
+         this.donorProfile = donorProf;
+         console.log(donorProf)
+       }
+      );
+      let responseDonor = this.http.get('http://smtb.mybluemix.net/get-project-donor?projectId=project001&donorId=surendra');
+    
+     responseDonor.subscribe(
+       (retValDonor) => {
+         this.responseDonor = retValDonor;
+         console.log(retValDonor)
+       }
+      );
+
   }
 
 }
